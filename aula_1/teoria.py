9fd562a5c655228171c88d106c8f380b5165ae25
professor <professor@SN-407013.edu.fiesc.com.br>
2026-04-22 21:55:22 -0300

    meu primeiro código em python

---
+++ aula_1/teoria.py
@@ -0,0 +1,15 @@
+# isso e um comentário
+
+#agora,nós escreveremos na tela usando o comando print!
+# o comando print, serve para escrevermos alguma coisa.
+
+print("Olá, Mundo!")
+print("Este é o curso de lógica de programação")
+
+# o comando input() serve para receber uma entrada
+# do teclado, sendo que o usuário irá digitar
+
+# A sintaxe do comando é input("mensagem")
+nome = input("digite seu nome:")
+
+print("Olá, ",nome )
